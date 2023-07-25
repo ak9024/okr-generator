61c49b5558c0ba99c1eec997d9b5f624b2342760
Adiatma Kamarudin <adiatma.mail@gmail.com>
2023-07-25 17:08:32 +0800

    feat(backend): update configuration and router setup
    In the  file in the  package, make the following changes:
    - Add new dependencies to the import list: , , , , and .
    - Create a new function  that returns a single instance of the  object using the sync.Once pattern.
    - Create a global variable  and a  object to ensure that only one instance of the  object is created.
    - Move the setup of the Swagger info to a new

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -1,7 +1,13 @@
 package router
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 
 	"github.com/ak9024/okr-generator/config"
 	"github.com/ak9024/okr-generator/docs"
@@ -12,7 +18,22 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
 	"github.com/gofiber/swagger"
+	"github.com/sirupsen/logrus"
 )
+
+var (
+	fiberApp *fiber.App
+	once     sync.Once
+)
+
+// single instance of the fiber.App
+func getFiberApp() *fiber.App {
+	once.Do(func() {
+		fiberApp = fiber.New()
+	})
+
+	return fiberApp
+}
 
 type server struct {
 	Config config.Provider
@@ -24,29 +45,64 @@ func NewServer(cfg config.Provider) *server {
 	}
 }
 
+func setupSwaggerInfo(s *server) {
+	// get hostname (host + port) get from file .config.toml
+	getHostName := fmt.Sprintf("%s:%d", s.Config.GetString("app.host"), s.Config.GetInt("app.port"))
+
+	// setup swagger info
+	docs.SwaggerInfo.Host = getHostName
+	docs.SwaggerInfo.Version = s.Config.GetString("app.version")
+}
+
 // @title OKR Generator API
 // @description This is Official API for OKR Generator API
-func (s *server) New() fiber.Router {
-	docs.SwaggerInfo.Host = fmt.Sprintf("%s:%d", s.Config.GetString("app.host"), s.Config.GetInt("app.port"))
-	docs.SwaggerInfo.Version = s.Config.GetString("app.version")
+func (s *server) New() {
+	setupSwaggerInfo(s)
 
-	app := fiber.New()
+	app := getFiberApp()
 
+	// setup fiber middleware
 	app.Use(cors.New())
 	app.Use(requestid.New())
 	app.Use(logger.New())
 
+	// declare endpoint for swagger documentation
 	app.Get("/swagger/*", swagger.HandlerDefault)
 
+	// init handler and service
 	service := service.NewService(s.Config)
 	h := handler.NewHandler(service)
 
 	api := app.Group("/api")
 	v1 := api.Group("/v1")
+
+	// POST /api/v1/okr-generator
 	v1.Post("/okr-generator", h.OKRGeneratorHandler)
 
-	port := fmt.Sprintf(":%d", s.Config.GetInt("app.port"))
-	app.Listen(port)
+	getPort := fmt.Sprintf(":%d", s.Config.GetInt("app.port"))
 
-	return app
+	if s.Config.GetString("app.env") == "development" {
+		if err := app.Listen(getPort); err != nil {
+			logrus.Error(err)
+		}
+	}
+
+	if s.Config.GetString("app.env") == "production" {
+		go func() {
+			if err := app.Listen(getPort); err != nil {
+				logrus.Error(err)
+			}
+		}()
+
+		quit := make(chan os.Signal)
+		signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+		<-quit
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		if err := app.ShutdownWithContext(ctx); err != nil {
+			logrus.Fatal(err)
+		}
+	}
 }
