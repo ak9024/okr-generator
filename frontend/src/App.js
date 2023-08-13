5094d69aed32e9ab7b3e7aebd73d79566f4a3a01
Adiatma Kamarudin <adiatma.mail@gmail.com>
2023-08-13 14:28:38 +0800

    chore(App): update App.js and index.js
    This commit updates the App.js and index.js files in the frontend/src directory.
    
    In App.js:
    - Reformat the import statements to use double quotes
    - Import the  hook from the  library
    - Define the  and  URLs using environment variables
    - Initialize  and  using the  hook
    - Define an  function to handle logout functionality and remove the token cookie
    - Check if the token cookie is defined, and if so, render a form with input fields for objective and translate
    - When submitting the form, send a POST request to the backend server API, passing the objective and translate values and the token cookie as authorization
    - If the token cookie is not defined, render a login link
    
    In index.js:
    - Reformat the import statements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useCookies } from "react-cookie";
+
+const logout = `${process.env.REACT_APP_BACKEND}/api/auth/google/logout`;
+const login = `${process.env.REACT_APP_BACKEND}/api/auth/google/login`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [cookies, removeCookies] = useCookies(["token"]);
+
+  const onClick = () => {
+    window.location.href = logout;
+    removeCookies("token");
+  };
+
+  if (cookies?.token !== "undefined") {
+    return (
+      <div className="App">
+        <form
+          onSubmit={(e) => {
+            let objective = e.target.objective.value;
+            let translate = e.target.translate.value;
+
+            console.log({ objective, translate });
+
+            fetch(`${process.env.REACT_APP_BACKEND}/api/v1/okr-generator`, {
+              method: "POST",
+              mode: "cors",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${cookies?.token}`,
+              },
+              body: JSON.stringify({
+                objective,
+                translate,
+              }),
+            })
+              .then((res) => res.json())
+              .then((res) => console.log(res));
+
+            e.preventDefault();
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <input type="text" name="objective" placeholder="objective" />
+          <input type="text" name="translate" placeholder="translate" />
+          <input type="submit" />
+        </form>
+        <button onClick={onClick}>logout</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <a href={login}>Login</a>
+      </div>
+    );
+  }
 }
 
 export default App;
