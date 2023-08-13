93486d2269cb78b1eae9b0f2fb4c99a5f95d1ac3
Adiatma Kamarudin <adiatma.mail@gmail.com>
2023-08-13 21:09:15 +0800

    feat(deploy): add HTTPS support for frontend deployment
    Add HTTPS support for the frontend deployment by exposing port 443 and configuring Nginx to handle HTTPS requests.
    
    - Modify the deploy.yml file to expose port 443 for the frontend container.
    - Add certbot as a dependency in the deploy.yml file to obtain SSL certificates for the domain malascoding.com.
    - Add a new nginx.conf file in the frontend/nginx directory to configure Nginx to handle HTTPS requests and serve the SSL certificates.
    - Update the frontend Dockerfile to copy the nginx.conf file to the appropriate location.
    - Update the App.js file to make an API call to the Google API endpoint to check if the user is authenticated and retrieve their profile information.
    - Update the App.js file to display the user's email if they are authenticated.
    - Update the App.js file to handle authentication errors and display an error message if necessary.
    
    This change enables secure communication between the frontend and client by using HTTPS, which improves the overall security

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,41 @@
 import "./App.css";
 import { useCookies } from "react-cookie";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const logout = `${process.env.REACT_APP_BACKEND}/api/auth/google/logout`;
 const login = `${process.env.REACT_APP_BACKEND}/api/auth/google/login`;
+const googleApis = `https://www.googleapis.com/oauth2/v2/userinfo`;
 
 function App() {
   const [cookies, removeCookies] = useCookies(["token"]);
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [authenticated, setAuthenticated] = useState(false);
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    fetch(`${googleApis}?access_token=${cookies?.token}`)
+      .then((res) => res.json())
+      .then((res) => {
+        if (profile?.error?.status !== "UNAUTHENTICATED") {
+          setProfile(res);
+          setAuthenticated(true);
+        } else {
+          setAuthenticated(false);
+        }
+      })
+      .catch((err) => setError(err));
+  }, [authenticated, cookies?.token]);
 
   const onClick = () => {
-    window.location.href = logout;
     removeCookies("token");
+    window.location.reload();
   };
 
-  if (cookies?.token !== "undefined") {
+  if (authenticated) {
     return (
-      <div className="App">
+      <div>
+        <p>Hello {profile?.email}</p>
         <form
           onSubmit={(e) => {
             let objective = e.target.objective.value;
@@ -41,9 +59,14 @@ function App() {
               .then((res) => {
                 setData(res);
                 setLoading(false);
+              })
+              .catch((err) => {
+                setError(err);
+                setLoading(false);
               });
 
             e.preventDefault();
+            e.currentTarget.reset();
           }}
         >
           <input type="text" name="objective" placeholder="objective" />
@@ -51,8 +74,9 @@ function App() {
           <input type="submit" disabled={loading} />
         </form>
         {loading && <p>Loading</p>}
+        {error && <p>{error}</p>}
         <div>
-          {(function() {
+          {(function () {
             if (data) {
               return (
                 <div>
