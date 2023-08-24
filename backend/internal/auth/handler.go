d62841ff574bf6cd4838a303189183c0e4a42fbc
Adiatma Kamarudin <adiatma.mail@gmail.com>
2023-08-24 08:51:50 +0800

    feat(backend): add same site mode none, and secure true

--- backend/internal/auth/handler.go
+++ backend/internal/auth/handler.go
@@ -51,10 +51,12 @@ func (a *auth) GoogleLoginCallback(c *fiber.Ctx) error {
 		}
 
 		c.Cookie(&fiber.Cookie{
-			Name:    "token",
-			Value:   token.AccessToken,
-			Expires: token.Expiry,
-			Domain:  a.Config.GetString("google.client_redirect_url"),
+			Name:     "token",
+			Value:    token.AccessToken,
+			Expires:  token.Expiry,
+			Domain:   a.Config.GetString("google.client_redirect_url"),
+			Secure:   true,
+			SameSite: fiber.CookieSameSiteNoneMode,
 		})
 
 		return c.Redirect(a.Config.GetString("google.client_redirect_url"), fiber.StatusTemporaryRedirect)
